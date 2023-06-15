cbb453336b17fc279ff2cbe749781e1f83219ac8
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-06-15 19:34:53 +0000

    Adding new file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - pritns a string
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
