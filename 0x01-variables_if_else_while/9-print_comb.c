052adaf43d6bd89e27a5e6245f5582cffb64b122
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-06-16 19:00:15 +0000

    Adding new file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digit numbers seperated by comma
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar((char)i);
+		if (i != '9')
+		{
+		putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
