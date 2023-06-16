68c694122d47d26b818278c3090c3d03cc8656d2
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-06-16 17:24:00 +0000

    Adding new file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers form 0 - 9
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+	return (0);
+}
