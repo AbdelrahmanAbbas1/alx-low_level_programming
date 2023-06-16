f01ae7af369b40f17c818a37cedced706cce43f5
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-06-16 18:47:43 +0000

    Adding new file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers 0 - 9
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '1'; i <= '9'; i++)
+	{
+		putchar((char)i);
+	}
+	putchar('\n');
+	return (0);
+}
