f18866fb4af4d83d4c4ec402ed6ebbe84c9bca07
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-06-22 13:22:14 +0000

    Adding new file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
  * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	char* name = "_putchar";
+	char *name = "_putchar";
 	int i;
 
 	for (i = 0; i < 8; i++)
 	{
-		putchar(name[i]);
+		_putchar(name[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
