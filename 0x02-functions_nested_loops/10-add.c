39b829751a376c9acebbd616215fce593d08108c
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-06-23 00:21:28 +0000

    Adding new file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - return the sum of the two numbers
+ * @a: the first argument
+ * @b: the second argument
+ * Return: return the sum of the two arguments
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
