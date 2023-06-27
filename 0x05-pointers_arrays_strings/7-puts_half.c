f6e0290c328cfee00e1964021106c39da68703f3
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-06-27 01:55:52 +0000

    Adding new file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str: The argument
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int i;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if ((len % 2) != 0)
+	{
+		i = ((len / 2) - 2);
+	}
+	else
+	{
+		i = len \ 2;
+	}
+	for (; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
