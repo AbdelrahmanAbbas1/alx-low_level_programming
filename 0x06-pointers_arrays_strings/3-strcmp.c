9039af09db1fbc19f5b48da6377851d4540cd4e9
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-06-29 18:24:15 +0000

    Adding new file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,23 +15,12 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] < s2[i])
 		{
-			return (-1);
+			return (s1[i] - s2[i]);
 		}
 		else if (s1[i] > s2[i])
 		{
-			return (1);
+			return (s1[i] - s2[i]);
 		}
 	}
-	if (s1[i] == '\0' && s2[i] == '\0')
-	{
-		return (0);
-	}
-	else if (s1[i] == '\0')
-	{
-		return (-1);
-	}
-	else
-	{
-	return (1);
-	}
+	return (0);
 }
