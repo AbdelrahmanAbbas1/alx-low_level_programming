d97346cfd2eab65055f6d7d46f43c734323bb8c2
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-06-29 17:39:46 +0000

    Adding new file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -22,5 +22,16 @@ int _strcmp(char *s1, char *s2)
 			return (1);
 		}
 	}
+	if (s1[i] == '\0')
+	{
+		return (-1);
+	}
+	else if (s2[i] == '\0')
+	{
+		return (1);
+	}
+	else
+	{
 	return (0);
+	}
 }
