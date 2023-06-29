ce51223b012a4cb85231c065419cbf57c5713226
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-06-29 16:32:23 +0000

    Adding new file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,22 +10,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int len = 0;
-	int len2 = 0;
 	int i;
 
-	while (src[len] != '\0')
-	{
-		len++;
-	}
-	while (dest[len2] != '\0')
-	{
-		len2++;
-	}
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	dest[len2] = '\0';
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 	return (dest);
 }
