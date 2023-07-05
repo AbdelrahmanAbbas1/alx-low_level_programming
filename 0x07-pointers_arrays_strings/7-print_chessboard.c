ed1f81f7c4d9157a66f36d6eb7cdb6a780db4347
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-07-05 02:04:50 +0000

    Adding new file

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints the chessboard
+ * @a: the first argument
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (k = 0; k < 8; k++)
+		{
+			_putchar(a[i][k]);
+		}
+		_putchar('\n');
+	}
+}
