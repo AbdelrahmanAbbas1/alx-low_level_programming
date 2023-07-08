9d5f06afedd911d1e41c1672cb7339646abd0d4b
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-07-08 12:55:07 +0000

    Adding new file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* @argc: Number of arguments
+* @argv: Number of strings
+*
+* Return: 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) > 0)
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
