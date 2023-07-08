1eb26136f3bd07e18adf89d37e8c445b4dbb8cb5
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-07-08 12:13:49 +0000

    Adding new file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: Array of strings
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
