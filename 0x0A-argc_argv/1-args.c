73c5b4f802096f8842f845fc62e7ddeae3724e2b
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-07-08 12:24:40 +0000

    Adding new file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* @argc: Number of arguments
+* @argv: Number of strings
+*
+* Return: 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
