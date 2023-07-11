99f1216455e8ad9e831fd8ef913ebb888123adaa
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-07-11 23:28:10 +0000

    Adding new file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,6 +16,14 @@ char *str_concat(char *s1, char *s2)
 	int len2 = 0;
 	char *a;
 
+	if (s1 == NULL)
+	{
+		s1[0] = ' ';
+	}
+	if (s2 == NULL)
+	{
+		s2[0] = ' ';
+	}
 	while (s1[len1] != '\0')
 	{
 		len1++;
