63c9961aa3acd07dd8cfe5045d2c7b4119abaa3a
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-07-13 19:06:49 +0000

    Adding new file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,10 +16,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len2 = 0;
 	char *a;
 
-	if (s2 == NULL || n == 0)
-	{
-		s2 = "";
-	}
 	while (s1[len1] != '\0')
 	{
 		len1++;
