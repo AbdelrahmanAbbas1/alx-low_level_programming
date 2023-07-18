a7ef332dd69ed48b8febe49c194d967272cafc2c
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-07-18 23:20:34 +0000

    Adding new file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees memory allocated dogs
+ * d: the structure variable to be freed
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
