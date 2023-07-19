6a14fc1cfc2320d4e163597e4258cfba2430a5f6
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-07-19 23:48:33 +0000

    Adding new file

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @ac: Arguments count
+ * @av: Argument strings
+ *
+ * Return: 0 (Sucess) or otherwise in error
+ */
+int main(int ac, char **av)
+{
+	int num1, num2;
+	char *op;
+
+	if (ac != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(av[1]);
+	op = av[2];
+	num2 = atoi(av[3]);
+
+	if (get_op_func == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds numbers
+ * @a: The first number
+ * @b: The seconde number
+ *
+ * Return: the result of the operation and otherwise if error
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Adds numbers
+ * @a: The first number
+ * @b: The seconde number
+ *
+ * Return: the result of the operation and otherwise if error
+ */
+int op_sub(int a, int b)
+{
+	if (a > b)
+		return (a - b);
+	else
+		return (b - a);
+}
+
+/**
+ * op_mul - Adds numbers
+ * @a: The first number
+ * @b: The seconde number
+ *
+ * Return: the result of the operation and otherwise if error
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Adds numbers
+ * @a: The first number
+ * @b: The seconde number
+ *
+ * Return: the result of the operation and otherwise if error
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Adds numbers
+ * @a: The first number
+ * @b: The seconde number
+ *
+ * Return: the result of the operation and otherwise if error
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
