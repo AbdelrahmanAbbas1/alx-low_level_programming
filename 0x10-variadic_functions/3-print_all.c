b1f558da6b9d26049386004bbeb7d33f9ff81acc
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-07-21 17:11:52 +0000

    Adding new file

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,39 +11,40 @@ void print_all(const char * const format, ...)
 	int c, x, i = 0;
 	va_list args;
 	double f;
-	char *s;
+	char *s, *sep = "";
 
 	va_start(args, format);
+	if (format != NULL)
+	{
 	while (format[i] != '\0')
 	{
 		switch (format[i])
 		{
 			case 'c':
 				c = va_arg(args, int);
-				printf("%c", c);
+				printf("%s%c", sep, c);
 				break;
 			case 'i':
 				x = va_arg(args, int);
-				printf("%d", x);
+				printf("%s%d", sep, x);
 				break;
 			case 'f':
 				f = va_arg(args, double);
-				printf("%f", f);
+				printf("%s%f", sep, f);
 				break;
 			case 's':
 				s = va_arg(args, char *);
 				if (s == NULL)
 					s = "(nil)";
-				printf("%s", s);
+				printf("%s%s", sep, s);
 				break;
 			default:
-				break;
+				i++;
+				continue;
 		}
-		if (format[i + 1] != '\0' &&
-		(format[i] == 'c' || format[i] == 'i' ||
-		 format[i] == 'f' || format[i] == 's'))
-			printf(", ");
+		sep = ", ";
 		i++;
+	}
 	}
 	printf("\n");
 	va_end(args);
