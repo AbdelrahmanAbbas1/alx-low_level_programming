1a80ede1f476298b61d873cc80b4b149f1f6cfb0
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-07-21 14:31:13 +0000

    Adding new file

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,6 +13,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
+
 	for (i = 0; i < n; i++)
 	{
 		char *cur_s = va_arg(args, char *);
@@ -23,4 +24,5 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s%s", cur_s == NULL ? "(nil)" : cur_s, separator);
 	}
 	printf("\n");
+	va_end(args);
 }
