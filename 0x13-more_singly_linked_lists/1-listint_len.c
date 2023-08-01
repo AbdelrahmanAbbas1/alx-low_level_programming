a4e21178c4f7df7271512816c038a130122b5e46
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-08-01 20:07:13 +0000

    Adding new file

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -5,7 +5,7 @@
  * listint_len - Returns the number of elements in a linked list
  * @h: A pointer to the listint_t
  *
- * Return the number of elements in a linked listint_t list
+ * Return: the number of elements in a linked listint_t list
  */
 size_t listint_len(const listint_t *h)
 {
