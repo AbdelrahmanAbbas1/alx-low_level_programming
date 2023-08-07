7f9defe046ba9ece343bc0874a1f9d809b781b51
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-08-07 17:02:00 +0000

    Adding new file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to the stdout
+ * @filename: A pointer to the file path
+ * @letters: The number of the letters it should read and print
+ *
+ * Return: The actual number of letters it could read and print or 0 otherwise
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t bytes_read;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1)
+	{
+		close (fd);
+		return (0);
+	}
+	free(buffer);
+	close (fd);
+	return (bytes_written);
+
+}
