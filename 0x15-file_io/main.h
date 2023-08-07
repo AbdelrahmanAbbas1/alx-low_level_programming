aee0379ca487a4331574862d70512b6da996c68f
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-08-07 17:02:48 +0000

    Updating the header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 
 #define MAIN_H
-
+#include <fcntl.h>
+#include <stddef.h>
+#include <unistd.h>
 int _putchar(int c);
+ssize_t read_textfile(const char *filename, size_t letters);
 #endif
