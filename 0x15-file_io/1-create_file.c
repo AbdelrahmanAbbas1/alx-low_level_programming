cda71f840dedeb0e06913ebe745be5178f0ffa1d
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-08-08 13:53:30 +0000

    Adding new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,11 +22,14 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	bytes_written = write(fd, text_content, i);
-	if (bytes_written == -1)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		bytes_written = write(fd, text_content, i);
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
