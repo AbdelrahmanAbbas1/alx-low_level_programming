15ae4e56cd10a1a0ddd5598679bc6c50ef39166b
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-08-09 16:37:04 +0000

    Adding new file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * close_fd - Closes a file
+ * @fd: the file descriptor
+ */
+void close_fd(int fd)
+{
+	int fd_v;
+
+	fd_v = close(fd);
+	if (fd_v == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
  * main - copies the content of a file to another file
  * @ac: Argument count
  * @av: Argument strings
