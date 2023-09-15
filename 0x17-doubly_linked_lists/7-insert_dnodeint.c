78c7a7609b8c5af3b89c3a583c04fb0c2149d290
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-09-15 20:08:28 +0000

    Adding new file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -43,9 +43,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			if (temp->next)
 				temp->next->prev = new_node;
 			temp->next = new_node;
+			return (new_node);
 		}
 		temp = temp->next;
 		i++;
 	}
-	return (new_node);
+	free(new_node);
+	return (NULL);
 }
