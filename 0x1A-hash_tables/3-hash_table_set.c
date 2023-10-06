7647c3bd727536aa217026838fd434deaa221620
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-10-06 20:47:22 +0000

    Adding new file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,26 +9,41 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int hash_index;
+	unsigned long int index, i;
 	char *value_copy;
+	hash_node_t *hash_node;
 
-	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
+	if (ht == NULL || key == NULL || value == NULL || *key =='\0')
 		return (0);
 
 	value_copy = strdup(value);
 	if (value_copy == NULL)
 		return (0);
 
-	hash_index = key_index((unsigned char *)key, ht->size);
+	index = key_index((unsigned char *)key, ht->size);
 
-	if (strcmp(ht->array[hash_index]->key, key) == 0)
+	for (i = index; ht->array[i]; i++)
 	{
-		free(ht->array[hash_index]->value);
-		ht->array[hash_index]->value = value_copy;
-		return (1);
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
 	}
-	ht->array[hash_index]->value = value_copy;
-	ht->array[hash_index]->key = strdup(key);
-
+	hash_node = malloc(sizeof(hash_node_t));
+	if (hash_node == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	hash_node->key = strdup(key);
+	if (hash_node->key == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	hash_node->value = value_copy;
+	hash_node->next = ht->array[index];
+	ht->array[index] = hash_node;
 	return (1);
 }
