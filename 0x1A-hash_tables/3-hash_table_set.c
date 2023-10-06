6bdd8b8fc09f600ff442c8da32cfab69a15de722
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2023-10-06 20:48:14 +0000

    Adding new file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *value_copy;
 	hash_node_t *hash_node;
 
-	if (ht == NULL || key == NULL || value == NULL || *key =='\0')
+	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
 		return (0);
 
 	value_copy = strdup(value);
